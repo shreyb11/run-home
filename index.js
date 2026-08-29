bbaee7427569853485bb9138e71e74321baebbd3
Shreya Bhatt <shreyabhatt2004@gmail.com>
2026-08-29 15:11:16 -0700

    Project set-up and player creation

---
+++ index.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        
+        this.width = 30
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+
+player.draw()
