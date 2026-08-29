6f401a84fd8994b2e73dca744d43a0d4969c3cde
Shreya Bhatt <shreyabhatt2004@gmail.com>
2026-08-29 16:50:11 -0700

    Player movement added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -37,11 +38,74 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    }
+    else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
 }
 
 animate()
+
+
+// Event Listeners
+
+window.addEventListener('keydown', ( { keyCode }) => {
+    switch (keyCode) { 
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+        
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20 
+            break
+    }
+})
+
+window.addEventListener('keyup', ( { keyCode }) => {
+    switch (keyCode) { 
+        case 65:
+            console.log('left stop')
+            keys.left.pressed = false
+            break
+
+        case 83:
+            console.log('down stop')
+            break
+
+        case 68:
+            console.log('right stop')
+            keys.right.pressed = false
+            break
+        
+        case 87:
+            console.log('up stop') 
+            break
+    }
+})
