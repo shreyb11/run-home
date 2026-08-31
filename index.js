e924a0a35111ec81f3487de7fffaaaf16fe24f66
Shreya Bhatt <shreyabhatt2004@gmail.com>
2026-08-31 12:35:48 -0700

    Multi platform + Win scenario + platform png

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+// img code possibly?
+const platformImg = new Image();
+platformImg.src = 'Platform.png'; 
+
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
@@ -45,10 +49,10 @@ class Player {
 
 // Platform class construction
 class Platform {
-    constructor() {
+    constructor({ x, y }) {
         this.position = {
-            x: 200,
-            y: 500
+            x: x,
+            y: y
         }
 
         this.width = 200
@@ -62,9 +66,7 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
-
-
+const platforms = [new Platform({x: 200, y: 300}), new Platform({x: 500, y: 500})]
 
 const keys = {
     right: {
@@ -75,28 +77,55 @@ const keys = {
     },
 }
 
+let scrollOffset = 0
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platform.draw()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
     }
-    else if (keys.left.pressed) {
+    else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5
-    } else player.velocity.x = 0
+    } else {
+        player.velocity.x = 0
 
-    // Platform collision detection
-    if (
-        player.position.y + player.height <= platform.position.y &&
-        player.position.y + player.height + player.velocity.y >= platform.position.y &&
-        player.position.x + player.width >= platform.position.x &&
-        player.position.x <= platform.position.x + platform.width
-    ) {
-        player.velocity.y = 0
+        // scroll effect
+        if (keys.right.pressed) {
+            scrollOffset += 5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
     }
+
+    
+    platforms.forEach(platform => {
+        // Platform collision detection
+        if (
+            player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width
+        ) {
+            player.velocity.y = 0
+        }
+
+        // Win scenario
+        if (scrollOffset > 2000) {
+            console.log('you win!')
+        }
+    })
 }
 
 animate()
